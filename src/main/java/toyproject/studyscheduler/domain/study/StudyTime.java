d5cac5ad5f6b0f4da9f4da4fef7015f615790030
jonghunbaek <qor6028@naver.com>
2023-08-16 20:16:56 +0900

    feat : StudyTime 필드 추가
    - 과거의 데이터를 저장할 때 날짜를 지정하기 위한 필드

--- src/main/java/toyproject/studyscheduler/domain/study/StudyTime.java
+++ src/main/java/toyproject/studyscheduler/domain/study/StudyTime.java
@@ -1,8 +1,15 @@
 package toyproject.studyscheduler.domain.study;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import toyproject.studyscheduler.domain.BaseEntity;
 
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class StudyTime extends BaseEntity {
 
@@ -13,6 +20,8 @@ public class StudyTime extends BaseEntity {
     private int totalCompleteTime;
 
     private int completeTimePerDay;
+
+    private LocalDate date;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Study study;
