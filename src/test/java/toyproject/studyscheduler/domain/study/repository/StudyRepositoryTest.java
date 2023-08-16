62369ea2b8b95572c6414e8b8d2e1bf6844df274
jonghunbaek <qor6028@naver.com>
2023-08-16 20:44:00 +0900

    docs : properties -> yml, application-test.yml
    - properties -> yml, 프로필 환경 설정
    - application-test.yml 생성

--- src/test/java/toyproject/studyscheduler/domain/study/repository/StudyRepositoryTest.java
+++ src/test/java/toyproject/studyscheduler/domain/study/repository/StudyRepositoryTest.java
@@ -4,14 +4,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
+@ActiveProfiles("test")
+@Transactional
 @DataJpaTest
 class StudyRepositoryTest {
 
     @Autowired
     StudyRepository studyRepository;
-
-
 
     @DisplayName("주어진 기간에 속한 학습내용을 모두 조회 한다.")
     @Test
