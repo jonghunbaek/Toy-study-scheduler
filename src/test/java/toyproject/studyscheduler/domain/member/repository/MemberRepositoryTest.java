6efd30e5997d10ef7087ee413af2853cef6db10d
jonghunbaek <qor6028@naver.com>
2023-08-25 18:57:33 +0900

    test : member의 정보 조회 테스트

--- src/test/java/toyproject/studyscheduler/domain/member/repository/MemberRepositoryTest.java
+++ src/test/java/toyproject/studyscheduler/domain/member/repository/MemberRepositoryTest.java
@@ -1,7 +1,13 @@
 package toyproject.studyscheduler.domain.member.repository;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import toyproject.studyscheduler.domain.member.AccountType;
+import toyproject.studyscheduler.domain.member.Member;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,4 +15,31 @@ import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 class MemberRepositoryTest {
 
+    @Autowired
+    MemberRepository memberRepository;
+
+    @DisplayName("아이디 값을 전달 받아 사용자의 정보를 조회한다.")
+    @Test
+    void test() {
+        // given
+        Member member = createMember();
+        memberRepository.save(member);
+
+        // when
+        Member result = memberRepository.findAll().get(0);
+
+        // then
+        Assertions.assertThat(result.getName()).isEqualTo("hong");
+     }
+
+    private static Member createMember() {
+        return Member.builder()
+            .email("hong@gmail.com")
+            .password("zxcv1234")
+            .name("hong")
+            .accountType(AccountType.ACTIVE)
+            .originProfileImage("1234")
+            .storedProfileImage("4151")
+            .build();
+    }
 }
