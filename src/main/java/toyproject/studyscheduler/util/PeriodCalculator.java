5a3f606b41257e03437d6f33fa3d9f4e273de7bb
jonghunbaek <qor6028@naver.com>
2023-09-29 16:54:29 +0900

    refactor : calculateTotalExpectedPeriod 로직 작성

--- src/main/java/toyproject/studyscheduler/util/PeriodCalculator.java
+++ src/main/java/toyproject/studyscheduler/util/PeriodCalculator.java
@@ -1,28 +1,28 @@
 package toyproject.studyscheduler.util;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
+import lombok.*;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 import static java.time.DayOfWeek.*;
 
-@Component
+/**
+ *  예상되는 학습 기간을 계산하는 클래스. StudyUtil에 의해서만 호출된다.
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class StudyPeriodUtil {
+public class PeriodCalculator {
 
     private int planTimeInWeekday;
     private int planTimeInWeekend;
     private LocalDate startDate;
 
-    public StudyPeriodUtil setUpBasicInfo(int planTimeInWeekday, int planTimeInWeekend, LocalDate startDate) {
+    @Builder(access = AccessLevel.PROTECTED)
+    private PeriodCalculator(int planTimeInWeekday, int planTimeInWeekend, LocalDate startDate) {
         this.planTimeInWeekday = planTimeInWeekday;
         this.planTimeInWeekend = planTimeInWeekend;
         this.startDate = startDate;
-        return this;
     }
 
     public int calculateTotalExpectedPeriod(int totalRunTime) {
@@ -37,15 +37,17 @@ public class StudyPeriodUtil {
 
             if (startDay <= FRIDAY.getValue()) {
                 totalRunTime -= planTimeInWeekday;
-                startDay = dayOfWeek.plus(1).getValue();
+                dayOfWeek = dayOfWeek.plus(1);
+                startDay = dayOfWeek.getValue();
                 period++;
                 continue;
             }
 
-            if (FRIDAY.getValue() < startDay && startDay <= SUNDAY.getValue()) {
-                startDay = dayOfWeek.plus(1).getValue();
+            if (startDay <= SUNDAY.getValue()) {
+                totalRunTime -= planTimeInWeekend;
+                dayOfWeek = dayOfWeek.plus(1);
+                startDay = dayOfWeek.getValue();
                 period++;
-                continue;
             }
         }
 
