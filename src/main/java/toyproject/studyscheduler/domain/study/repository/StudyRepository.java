7e19190eed56197314034d3b22a7b79653b89078
jonghunbaek <qor6028@naver.com>
2023-10-04 20:51:41 +0900

    docs : todo 추가

--- src/main/java/toyproject/studyscheduler/domain/study/repository/StudyRepository.java
+++ src/main/java/toyproject/studyscheduler/domain/study/repository/StudyRepository.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
 
+    // TODO : expectedEndDate -> realEndDate로 변경하기
     @Query("select s from Study s where (s.startDate >= :startDate and s.startDate <= :endDate)" +
         "or (s.expectedEndDate >= :startDate and s.expectedEndDate <= :endDate) ")
     List<Study> findAllByPeriod(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
