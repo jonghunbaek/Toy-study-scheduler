0a85057735ee5fae6e7404454981c3c1f0953269
jonghunbaek <qor6028@naver.com>
2023-10-28 11:29:33 +0900

    feat : controller 요청 매핑 메서드 추가

--- src/main/java/toyproject/studyscheduler/api/controller/StudyController.java
+++ src/main/java/toyproject/studyscheduler/api/controller/StudyController.java
@@ -2,11 +2,13 @@ package toyproject.studyscheduler.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import toyproject.studyscheduler.api.controller.request.SaveStudyRequestDto;
+import toyproject.studyscheduler.api.controller.request.StudyPlanTimeRequestDto;
+import toyproject.studyscheduler.api.controller.response.FindStudyResponseDto;
 import toyproject.studyscheduler.api.service.study.StudyFactory;
 import toyproject.studyscheduler.api.service.study.StudyService;
+import toyproject.studyscheduler.domain.study.StudyType;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,5 +25,19 @@ public class StudyController {
     public void saveStudy(SaveStudyRequestDto saveStudyRequestDto) {
         StudyService studyService = studyFactory.findServiceBy(saveStudyRequestDto.getStudyType());
         studyService.saveStudy(saveStudyRequestDto);
+    }
+
+    @ResponseBody
+    @GetMapping("/studies/{id}")
+    public FindStudyResponseDto getStudyBy(@PathVariable Long id, @RequestHeader StudyType studyType) {
+        StudyService studyService = studyFactory.findServiceBy(studyType);
+        return studyService.findStudyById(id);
+    }
+
+    @ResponseBody
+    @GetMapping("/studies/period")
+    public int getPeriod(@ModelAttribute StudyPlanTimeRequestDto dto) {
+        StudyService studyService = studyFactory.findServiceBy(dto.getStudyType());
+        return studyService.calculatePeriod(dto);
     }
 }
