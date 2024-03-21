76c1dd540c32ffcb58a74a796782d9540ebf64c5
jonghunbaek <qor6028@naver.com>
2024-03-21 15:52:23 +0900

    feat : 회원 가입, 로그인 웹계층 생성 및 로그인 시 토큰 반환

--- src/main/java/toyproject/studyscheduler/auth/web/AuthController.java
+++ src/main/java/toyproject/studyscheduler/auth/web/AuthController.java
@@ -1,12 +1,31 @@
 package toyproject.studyscheduler.auth.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import toyproject.studyscheduler.auth.application.AuthService;
+import toyproject.studyscheduler.auth.application.TokenService;
+import toyproject.studyscheduler.auth.application.dto.MemberInfo;
+import toyproject.studyscheduler.auth.application.dto.SignInInfo;
+import toyproject.studyscheduler.auth.application.dto.SignUpInfo;
+import toyproject.studyscheduler.auth.web.dto.Tokens;
 
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
 
     private final AuthService authService;
+    private final TokenService tokenService;
+
+    @PostMapping("/auth/sign-up")
+    public void signUp(@RequestBody SignUpInfo signUpInfo) {
+        authService.saveNewMember(signUpInfo);
+    }
+
+    @PostMapping("/auth/sign-in")
+    public Tokens signIn(@RequestBody SignInInfo signInInfo) {
+        MemberInfo memberInfo = authService.login(signInInfo);
+        return tokenService.createTokens(memberInfo);
+    }
 }
