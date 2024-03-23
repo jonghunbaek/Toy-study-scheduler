0e886b3dfa643d7cc8b1c6b15d3f58f2a49fe193
jonghunbaek <qor6028@naver.com>
2024-03-23 19:24:45 +0900

    feat : token으로 엔티티 찾는 메서드 추가

--- src/main/java/toyproject/studyscheduler/token/repository/RefreshTokenRepository.java
+++ src/main/java/toyproject/studyscheduler/token/repository/RefreshTokenRepository.java
@@ -3,5 +3,9 @@ package toyproject.studyscheduler.token.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import toyproject.studyscheduler.token.entity.RefreshToken;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String refreshToken);
 }
---
+++ src/test/java/toyproject/studyscheduler/token/repository/RefreshTokenRepositoryTest.java
@@ -0,0 +1,34 @@
+package toyproject.studyscheduler.token.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import toyproject.studyscheduler.token.entity.RefreshToken;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class RefreshTokenRepositoryTest {
+
+    @Autowired
+    RefreshTokenRepository refreshTokenRepository;
+
+    @DisplayName("토큰과 일치하는 리프레쉬 토큰 엔티티를 가져온다.")
+    @Test
+    void findByToken() {
+        // given
+        String token = "12345";
+        RefreshToken refreshToken = new RefreshToken(1L, token);
+        refreshTokenRepository.save(refreshToken);
+
+        // when
+        RefreshToken findToken = refreshTokenRepository.findByToken(token)
+                .orElseThrow();
+
+        // then
+        assertThat(findToken.getToken()).isEqualTo(token);
+    }
+}
