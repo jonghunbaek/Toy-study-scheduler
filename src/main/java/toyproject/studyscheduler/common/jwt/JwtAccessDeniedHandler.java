a8c24785afa2382cd0c24089bcd5edb1ddc970d6
jonghunbaek <qor6028@naver.com>
2024-03-27 13:25:53 +0900

    feat : 접근권한이 없는 경우에 대한 예외 처리

--- src/main/java/toyproject/studyscheduler/common/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/toyproject/studyscheduler/common/jwt/JwtAccessDeniedHandler.java
@@ -1,11 +1,15 @@
 package toyproject.studyscheduler.common.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
+import toyproject.studyscheduler.common.response.ResponseCode;
+import toyproject.studyscheduler.common.response.ResponseForm;
 
 import java.io.IOException;
 
@@ -14,6 +18,10 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/json; charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(ResponseForm.of(ResponseCode.E00001)));
     }
 }
