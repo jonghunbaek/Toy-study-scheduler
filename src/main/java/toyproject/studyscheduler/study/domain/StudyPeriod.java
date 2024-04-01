94cb53e0f80497d1eae7ee20ffde97ef1133c6ad
jonghunbaek <qor6028@naver.com>
2024-04-01 15:02:51 +0900

    feat : 종료 여부에 따른 정적 팩토리 메서드 추가
    - 새로 시작하는 스터디 기간의 종료일은 최소값으로 설정
    - 관련 테스트 작성

--- src/main/java/toyproject/studyscheduler/study/domain/StudyPeriod.java
+++ src/main/java/toyproject/studyscheduler/study/domain/StudyPeriod.java
@@ -2,11 +2,12 @@ package toyproject.studyscheduler.study.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
-import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class StudyPeriod {
@@ -14,9 +15,16 @@ public class StudyPeriod {
     private LocalDate startDate;
     private LocalDate endDate;
 
-    @Builder
     private StudyPeriod(LocalDate startDate, LocalDate endDate) {
         this.startDate = startDate;
         this.endDate = endDate;
+    }
+
+    public static StudyPeriod fromStarting(LocalDate startDate) {
+        return new StudyPeriod(startDate, LocalDate.MIN);
+    }
+
+    public static StudyPeriod fromTerminated(LocalDate startDate, LocalDate endDate) {
+        return new StudyPeriod(startDate, endDate);
     }
 }
---
+++ src/test/java/toyproject/studyscheduler/study/domain/StudyPeriodTest.java
@@ -0,0 +1,19 @@
+package toyproject.studyscheduler.study.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
+
+class StudyPeriodTest {
+
+    @DisplayName("시작할 스터디 기간 생성시 종료일은 최소 값으로 설정한다.")
+    @Test
+    void fromStarting() {
+        StudyPeriod studyPeriod = StudyPeriod.fromStarting(LocalDate.now());
+
+        assertThat(studyPeriod.getEndDate()).isEqualTo(LocalDate.MIN);
+    }
+}
