626526f0ba5d5474f875d0e6d348e38d81513743
jonghunbaek <qor6028@naver.com>
2024-04-03 12:31:33 +0900

    test : 미사용 테스트 제거 및 테스트 코드 리팩토링

---
+++ src/test/java/toyproject/studyscheduler/study/domain/PeriodCalculatorTest.java
@@ -0,0 +1,75 @@
+package toyproject.studyscheduler.study.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import toyproject.studyscheduler.study.domain.PeriodCalculator;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PeriodCalculatorTest {
+
+    @DisplayName("강의 학습의 총 예상 기간을 계산한다.")
+    @Test
+    void calculateTotalExpectedPeriodWithLecture() {
+        // given
+        int planTimeInWeekDay = 60;
+        int planTimeInWeekend = 120;
+        int totalRunTime = 600;
+        LocalDate startDate = LocalDate.of(2023, 9, 15);
+        PeriodCalculator periodCalculator = new PeriodCalculator(planTimeInWeekDay, planTimeInWeekend, startDate);
+
+        // when
+        int period = periodCalculator.calculateExpectedPeriod(600);
+
+        // then
+        assertThat(period).isEqualTo(8);
+    }
+
+    @DisplayName("월요일에 시작한 경우 독서 학습의 총 예상 기간을 계산한다.")
+    @Test
+    void calculateTotalExpectedPeriodWithReadingMonday() {
+        // given
+        int planTimeInWeekDay = 30;
+        int planTimeInWeekend = 60;
+        int totalPage = 700;
+        int readPagePerMin = 2;
+        LocalDate startDate = LocalDate.of(2023, 9, 11);
+
+        PeriodCalculator periodCalculator = new PeriodCalculator(
+            planTimeInWeekDay * readPagePerMin,
+            planTimeInWeekend * readPagePerMin,
+            startDate);
+
+        // when
+        int period = periodCalculator.calculateExpectedPeriod(totalPage);
+
+        // then
+        assertThat(period).isEqualTo(10);
+    }
+
+    @DisplayName("토요일에 시작한 경우 독서 학습의 총 예상 기간을 계산한다.")
+    @Test
+    void calculateTotalExpectedPeriodWithReadingSaturday() {
+        // given
+        int planTimeInWeekDay = 30;
+        int planTimeInWeekend = 60;
+        int totalPage = 700;
+        int readPagePerMin = 2;
+        LocalDate startDate = LocalDate.of(2023, 9, 16);
+
+        PeriodCalculator periodCalculator = new PeriodCalculator(
+            planTimeInWeekDay * readPagePerMin,
+            planTimeInWeekend * readPagePerMin,
+            startDate);
+
+        // when
+        int period = periodCalculator.calculateExpectedPeriod(totalPage);
+
+        // then
+        assertThat(period).isEqualTo(9);
+    }
+}
--- src/test/java/toyproject/studyscheduler/studytest/DayOfWeekTest.java
+++ src/test/java/toyproject/studyscheduler/studytest/DayOfWeekTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DayOfWeekTest {
 
@@ -19,5 +22,26 @@ public class DayOfWeekTest {
 
         // then
         Assertions.assertThat(dayOfWeek).isEqualTo(DayOfWeek.MONDAY);
+    }
+
+    @DisplayName("DayOfWeek 학습 테스트")
+    @Test
+    void dayOfWeekStudyTest() {
+        // given
+        LocalDate localDate = LocalDate.of(2023, 9,29);
+
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+
+        // when
+        DayOfWeek plus1 = dayOfWeek.plus(1);
+        DayOfWeek plus2 = plus1.plus(1);
+        DayOfWeek plus3 = plus2.plus(1);
+        DayOfWeek plus4 = plus3.plus(1);
+
+        // then
+        assertThat(plus1.getValue()).isEqualTo(6);
+        assertThat(plus2.getValue()).isEqualTo(7);
+        assertThat(plus3.getValue()).isEqualTo(1);
+        assertThat(plus4.getValue()).isEqualTo(2);
     }
 }
