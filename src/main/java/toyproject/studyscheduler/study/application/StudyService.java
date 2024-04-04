aa339bd0bef649c09e02845e490f31174eb34366
jonghunbaek <qor6028@naver.com>
2024-04-04 12:42:56 +0900

    test : 학습 저장 서비스 레이어 테스트 케이스 추가

--- src/main/java/toyproject/studyscheduler/study/application/StudyService.java
+++ src/main/java/toyproject/studyscheduler/study/application/StudyService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import toyproject.studyscheduler.study.application.dto.LectureSaveDto;
+import toyproject.studyscheduler.study.application.dto.StudySaveSpec;
 import toyproject.studyscheduler.study.domain.entity.Study;
 import toyproject.studyscheduler.study.repository.StudyRepository;
 
@@ -17,10 +18,9 @@ public class StudyService {
     private final StudyRepository studyRepository;
 
     // TODO :: 학습 저장시 다형성 활용할 수 있는지 시도해보기
-    public Study createStudy(LectureSaveDto lectureSaveDto) {
-        Study entity = lectureSaveDto.toEntity();
-        studyRepository.save(entity);
-        return null;
-    }
+    public Study createStudy(StudySaveSpec studySaveDto) {
+        Study entity = studySaveDto.toStudy();
 
+        return studyRepository.save(entity);
+    }
 }
--- src/test/java/toyproject/studyscheduler/study/application/StudyServiceTest.java
+++ src/test/java/toyproject/studyscheduler/study/application/StudyServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import toyproject.studyscheduler.study.application.dto.LectureSaveDto;
+import toyproject.studyscheduler.study.application.dto.ReadingSaveDto;
 import toyproject.studyscheduler.study.domain.entity.Study;
 
 import java.time.LocalDate;
@@ -23,7 +24,7 @@ class StudyServiceTest {
 
     @DisplayName("종료된 강의 학습을 생성한다.")
     @Test
-    void createLecture() {
+    void createLectureWhenTerminated() {
         // given
         LectureSaveDto lectureSaveDto = LectureSaveDto.builder()
             .title("김영한의 Spring")
@@ -38,9 +39,80 @@ class StudyServiceTest {
             .build();
 
         // when
-        Study study = studyService.createStudy(lectureSaveDto);
+        Study savedStudy = studyService.createStudy(lectureSaveDto);
 
         // then
-        assertThat(study.getStudyInformation().getTitle()).isEqualTo("김영한의 Spring");
+        assertThat(savedStudy.getStudyInformation().getTitle()).isEqualTo("김영한의 Spring");
+    }
+
+    @DisplayName("시작할 강의 학습을 생성한다.")
+    @Test
+    void createLectureWhenStarting() {
+        // given
+        LectureSaveDto lectureSaveDto = LectureSaveDto.builder()
+            .title("김영한의 Spring")
+            .description("Spring강의")
+            .isTermination(false)
+            .startDate(LocalDate.of(2024, 4, 1))
+            .planMinutesInWeekday(30)
+            .planMinutesInWeekend(60)
+            .teacherName("김영한")
+            .totalRuntime(500)
+            .build();
+
+        // when
+        Study savedStudy = studyService.createStudy(lectureSaveDto);
+
+        // then
+        assertThat(savedStudy.getStudyInformation().getTitle()).isEqualTo("김영한의 Spring");
+        assertThat(savedStudy.getStudyPeriod().getEndDate()).isEqualTo(LocalDate.MAX);
+    }
+
+    @DisplayName("종료된 강의 학습을 생성한다.")
+    @Test
+    void createReadingWhenTerminated() {
+        // given
+        ReadingSaveDto lectureSaveDto = ReadingSaveDto.builder()
+            .title("클린 코드")
+            .description("클린 코드 작성 방법")
+            .isTermination(true)
+            .startDate(LocalDate.of(2024, 4, 1))
+            .endDate(LocalDate.of(2024, 4, 21))
+            .planMinutesInWeekday(30)
+            .planMinutesInWeekend(60)
+            .authorName("로버트 마틴")
+            .totalPage(600)
+            .readPagePerMin(2)
+            .build();
+
+        // when
+        Study savedStudy = studyService.createStudy(lectureSaveDto);
+
+        // then
+        assertThat(savedStudy.getStudyInformation().getTitle()).isEqualTo("클린 코드");
+    }
+
+    @DisplayName("시작할 강의 학습을 생성한다.")
+    @Test
+    void createReadingWhenStarting() {
+        // given
+        ReadingSaveDto lectureSaveDto = ReadingSaveDto.builder()
+            .title("클린 코드")
+            .description("클린 코드 작성 방법")
+            .isTermination(false)
+            .startDate(LocalDate.of(2024, 4, 1))
+            .planMinutesInWeekday(30)
+            .planMinutesInWeekend(60)
+            .authorName("로버트 마틴")
+            .totalPage(600)
+            .readPagePerMin(2)
+            .build();
+
+        // when
+        Study savedStudy = studyService.createStudy(lectureSaveDto);
+
+        // then
+        assertThat(savedStudy.getStudyInformation().getTitle()).isEqualTo("클린 코드");
+        assertThat(savedStudy.getStudyPeriod().getEndDate()).isEqualTo(LocalDate.MAX);
     }
 }
