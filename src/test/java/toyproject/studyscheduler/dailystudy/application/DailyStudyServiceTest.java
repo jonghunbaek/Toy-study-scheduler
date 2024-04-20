99faddb76be74c7d8e3aaddfb750b2dc97e14a2e
jonghunbaek <qor6028@naver.com>
2024-04-20 11:44:47 +0900

    test : 일일 학습 수행일이 학습 시작일보다 빠른지 검증하는 테스트 작성

--- src/test/java/toyproject/studyscheduler/dailystudy/application/DailyStudyServiceTest.java
+++ src/test/java/toyproject/studyscheduler/dailystudy/application/DailyStudyServiceTest.java
@@ -17,6 +17,7 @@ import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static toyproject.studyscheduler.common.exception.GlobalException.DETAIL_DELIMITER;
 
 @ActiveProfiles("test")
 @Transactional
@@ -30,16 +31,30 @@ class DailyStudyServiceTest {
 
     @DisplayName("종료된 학습의 일일 학습을 생성하면 예외가 발생한다.")
     @Test
-    void createDailyStudy() {
+    void createDailyStudyAfterValidateTermination() {
         // given
-        LectureSave lecture1 = createLectureSave("김영한의 Spring", true, LocalDate.of(2024, 4, 1), LocalDate.of(2024, 4, 21));
-        studyService.createStudy(lecture1, 1L);
+        LectureSave lecture = createLectureSave("김영한의 Spring", true, LocalDate.of(2024, 4, 1), LocalDate.of(2024, 4, 21));
+        studyService.createStudy(lecture, 1L);
         DailyStudySave dailyStudySave = new DailyStudySave(1L, "오늘 학습한 내용", 30, LocalDate.now());
 
         // when & then
         assertThatThrownBy(() -> dailyStudyService.createDailyStudy(dailyStudySave))
             .isInstanceOf(StudyException.class)
             .hasMessage("해당 학습은 이미 종료되었습니다.");
+    }
+
+    @DisplayName("일일 학습의 수행일이 학습 시작일 보다 빠르면 예외가 발생한다.")
+    @Test
+    void createDailyStudyAfterValidateStudyDate() {
+        // given
+        LectureSave lecture = createLectureSave("김영한의 Spring", false, LocalDate.of(2024, 4, 1), null);
+        studyService.createStudy(lecture, 1L);
+        DailyStudySave dailyStudySave = new DailyStudySave(1L, "오늘 학습한 내용", 30, LocalDate.of(2024, 3, 31));
+
+        // when & then
+        assertThatThrownBy(() -> dailyStudyService.createDailyStudy(dailyStudySave))
+            .isInstanceOf(StudyException.class)
+            .hasMessage("학습 수행일이 학습 시작일 보다 빠릅니다." + DETAIL_DELIMITER + "studyDate :: " + "2024-03-31");
     }
 
     private LectureSave createLectureSave(String title, boolean isTermination, LocalDate startDate, LocalDate endDate) {
